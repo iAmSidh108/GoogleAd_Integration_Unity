ddb15cfabf48a501a4efd5a0b9d9abed364cb454
Siddharth Tiwari <sidh589@gmail.com>
2023-02-20 12:26:05 +0530

    Created Banner Ad using AdMob

---
+++ Dev_AdMobIntegration/Assets/Scripts/BannerAds.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class BannerAds : MonoBehaviour
+{
+    private string _adUnitId;
+    private BannerView _bannerView;
+    private AdRequest adRequest;
+
+
+    void Start()
+    {
+        MobileAds.Initialize((InitializationStatus initStat) =>
+        {
+            LoadBannerAds();
+        });
+
+    }
+    private void LoadBannerAds()
+    {
+        #if UNITY_ANDROID
+ _adUnitId = "ca-app-pub-3940256099942544/6300978111";
+#elif UNITY_IPHONE
+ _adUnitId = "ca-app-pub-3940256099942544/2934735716";
+#else
+  _adUnitId = "unused";
+#endif
+    _bannerView = new BannerView(_adUnitId, AdSize.Banner, AdPosition.Top);
+        adRequest = new AdRequest.Builder()
+        .AddKeyword("unity-admob-sample")
+        .Build();
+        Debug.Log("Loading banner ad.");
+        _bannerView.LoadAd(adRequest);
+        Debug.Log("Successfully Showed ad");
+    }
+}
